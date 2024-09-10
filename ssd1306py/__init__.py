f43b47415d6f5dbe4704b305dc05de909bf4c6cc
Dedicatus1979 <88102275+Dedicatus1979@users.noreply.github.com>
2024-09-10 19:28:49 +0800

    Delete ssd1306py directory

--- ssd1306py/__init__.py
+++
@@ -1,9 +0,0 @@
-# MicroPython SSD1306 OLED driver, I2C and SPI interfaces created by Adafruit
-# Copyright 2021-2021 The jdh99 Authors. All rights reserved.
-# ssd1306操作封装.支持多种英文字库
-# Authors: jdh99 <jdh821@163.com>
-# Make corrections and deletions with Dedicatus1979 at 2024-03-06
-
-from ssd1306py.ops import *
-
-__version__ = '3.1'
